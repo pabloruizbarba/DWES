48b8a4c8eb12ce05e969f11b9b126fbda4dbefd8
pabloruizbarba <pruizb@fpcoruna.afundacion.org>
2022-10-03 12:33:16 +0200

    Ejercicio 6, conversor2.php con yardas

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -8,7 +8,11 @@
 				if ($_POST["funidad"] == "pulgada") {
 					$v_pulgadas = $_POST["fcantidad"];
 					$v_metros = $v_pulgadas * 0.0254;
-					echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+					echo $v_pulgadas." pulgada(s) = ".$v_metros." metro(s)";
+				} elseif ($_POST["funidad"] == "yarda") {
+					$v_yardas = $_POST["fcantidad"];
+					$v_metros = $v_yardas * 0.9144;
+					echo $v_yardas." yarda(s) = ".$v_metros." metro(s)";
 				} else {
 					echo "Unidad no soportada";
 				}
@@ -22,6 +26,8 @@
 
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for="yarda_input">Yarda(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 
