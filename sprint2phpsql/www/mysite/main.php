ae11e87bcf72b73160d16e0342f3d211364eb6dc
pabloruizbarba <pruizb@fpcoruna.afundacion.org>
2022-10-11 14:39:56 +0200

    Ejercicio 2

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,4 +1,48 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<head>
+		<style>
+			table,tr,td{
+				border: 1px solid navy;
+			}
+			th{
+				background-color: lime;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Conexión establecida</h1>
+		<?php
+			// Lanzar una query
+			$query = 'SELECT * FROM tPeliculas';
+			$result = mysqli_query($db, $query) or die('Query error');
+			echo '<table>';
+			echo '<tr><th>id</th><th>Nombre</th><th>Imagen</th><th>Director</th><th>Año</th></tr>';
+			// Recorrer el resultado
+			while ($row = mysqli_fetch_array($result)) {
+				echo '<tr><td>';
+				echo $row['id'];
+				echo '</td>'; 
+				echo '<td>';
+				echo $row['nombre'];
+				echo '</td>';
+				echo '<td><a href="/detail.php?id=';
+				echo $row[0];
+				echo '">';
+				echo '<img width="200px" height="300px" src="';
+				echo $row['url_imagen'];
+				echo '"></a></td>';
+				echo '<td>';
+				echo $row['director'];
+				echo '</td>';
+				echo '<td>';
+				echo $row['anyo'];
+				echo '</td></tr>';
+			}
+			echo '</table>';
+			mysqli_close($db);
+		?>
+	</body>
+</html>
