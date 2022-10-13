3d1d63dcdc55e72605d407c03297518d1a5b14e1
pabloruizbarba <pruizb@fpcoruna.afundacion.org>
2022-10-13 13:19:35 +0200

    Ejercicio 5

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -26,9 +26,9 @@
 		?>
 	</ul>
 	<p>Deja un nuevo comentario:</p>
-	<form actions="/comment.php" method="post">
+	<form action="/comment.php" method="post">
 		<textarea rows="4" cols="50" name="new_comment"></textarea><br>
-		<input type="hidden" name="pelicula_id" value="<php? echo $pelicula_id; ?>">
+		<input type="hidden" name="pelicula_id" value="<?php echo $pelicula_id; ?>">
 		<input type="submit" value="Comentar">
 	</form>
 </body>
