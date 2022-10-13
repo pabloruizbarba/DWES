23e6c680592befb01e06b55d649b3a4b47c8410e
Pablo <pruizb@fpcoruna.afundacion.org>
2022-10-13 14:22:24 +0200

    Ejercicio 1, concurso

---
+++ sprint2python/concurso.py
@@ -0,0 +1,9 @@
+print("¿Cuántos Oscar ha ganado 'El retorno del Rey'?")
+print("a) 12")
+print("b) 11")
+print("c) 13")
+respuesta=input("Tu respuesta es: ")
+if respuesta=="11":
+    print("¡Has acertado!")
+else:
+    print("Has fallado")
