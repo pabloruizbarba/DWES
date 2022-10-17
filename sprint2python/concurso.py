cffec243c2f37177cb39a75c373915e456acee61
Pablo <pruizb@fpcoruna.afundacion.org>
2022-10-17 12:37:06 +0200

    Ejercicio 2

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,3 +1,4 @@
+puntos=0
 print("¿Cuántos Oscar ha ganado 'El retorno del Rey'?")
 print("a) 12")
 print("b) 11")
@@ -5,5 +6,30 @@ print("c) 13")
 respuesta=input("Tu respuesta es (a/b/c): ")
 if respuesta=="11" or respuesta.lower()=="b":
     print("¡Has acertado!")
+    puntos = puntos + 10
 else:
     print("Has fallado")
+    puntos = puntos - 5
+print("¿Cuántos años tiene Jack Nicholson?")
+print("a) 85")
+print("b) 86")
+print("c) 87")
+respuesta=input("Tu respuesta es (a/b/c): ")
+if respuesta=="85" or respuesta.lower()=="a":
+    print("¡Has acertado!")
+    puntos = puntos + 10
+else:
+    print("Has fallado")
+    puntos = puntos - 5
+print("¿Como se llama actualmente la capital de Kazajistán?")
+print("a) Akmola")
+print("b) Astaná")
+print("c) Nursultán")
+respuesta=input("Tu respuesta es (a/b/c): ")
+if respuesta=="Nursultán" or respuesta.lower()=="c":
+    print("¡Has acertado!")
+    puntos = puntos + 10
+else:
+    print("Has fallado")
+    puntos = puntos - 5
+print("Tu puntuación final es: ",puntos)
