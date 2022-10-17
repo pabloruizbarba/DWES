f689218792e293586ac0ccd52078e5f174191e04
Pablo <pruizb@fpcoruna.afundacion.org>
2022-10-17 12:46:54 +0200

    Ejercicio 4

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,7 +1,7 @@
-from factorial import func_factorial
+from factorial2 import func_factorial2
 
 num = int(input("Introduzca un número positivo: "))
 if num < 0:
     print("Ese número es negativo, no es válido.")
 else:
-    print("El factorial de ",num," es: ",func_factorial(num))
+    print("El factorial de ",num," es: ",func_factorial2(num))
